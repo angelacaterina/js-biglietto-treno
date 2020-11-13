da932149b36bf08b1f077f5419bda9536b226b98
angelacaterina <angela.caterina@icloud.com>
2020-11-13 18:18:03 +0100

    3 metodi altrenativi
    é stato commentato il testo del html, perchè da consegna non era richiesto; e sono stati commentati sia PROGRAMMA 2 che PROGRAMMA 3.
    
    PROGRAMMA 1:  avviene solo una visualizzazione su console, e le variabili sono state tutte dichiarate esternamente al programma e poi richiamate al suo interno.
    
    PROGRAMMA 2: avviene sia una visualizzazione su console che sulla pagina web; anche qui le variabili sono state dichiarate esternamente, mentre la modifica dell' id è stata fatta internamente al programma per la visualizzazione sulla pagina web. 
    
    PROGRAMMA 3: avviene sia una visualizzazione su console che sulla pagina web; qui le variabili sono state dichiarate internamente al programma insieme alle modifiche dell' id per la visualizzazione sulla pagina web .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,111 @@
+////PROGRAMMA 1 CONSOLE////
 //1. Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 var kmDaPercorrere = Number(prompt("numero di chilometri che vuole percorrere"));
 //console.log(kmDaPercorrere);
-document.getElementById('kmDaPercorrere').innerHTML = kmDaPercorrere + "km";
 
 var userAge = Number(prompt("Quanti anni hai?"));
 //console.log(userAge);
-document.getElementById('userAge').innerHTML = userAge + " anni";
-
 
 //2. calcolare il prezzo totale del viaggio, secondo queste regole:
-
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 var prezzoBiglietto = Number(kmDaPercorrere * 0.21);
-document.getElementById('prezzoBiglietto').innerHTML = prezzoBiglietto + " € ";
 console.log(prezzoBiglietto);
 
-//////variabili utilizzate poi nel PROGRAMMA//////
 // va applicato uno sconto del 20% per i minorenni
-// var sconto20 = Number(prezzoBiglietto - (prezzoBiglietto * (20 / 100)));
+var sconto20 = Number(prezzoBiglietto - (prezzoBiglietto * (20 / 100)));
 // console.log(sconto20);
 
 // va applicato uno sconto del 40% per gli over 65
-// var sconto40 = Number(prezzoBiglietto - (prezzoBiglietto * (40 / 100)));
+var sconto40 = Number(prezzoBiglietto - (prezzoBiglietto * (40 / 100)));
 // console.log(sconto40);
 
-//PROGRAMMA
+
+// Variabili dichiarate esternamente al programma
 if(userAge <= 17) {
-  var sconto20 = Number(prezzoBiglietto - (prezzoBiglietto * (20 / 100)));
+  sconto20;
   console.log(sconto20);
-
-  document.getElementById('totalPrice').innerHTML = sconto20 + " € " + "sconto del 20%";
   console.log("Applicato uno sconto del 20%");
-
 } else if(userAge > 17 && userAge < 65) {
-  document.getElementById('totalPrice').innerHTML = prezzoBiglietto + " €";
+  prezzoBiglietto;
+  console.log(prezzoBiglietto);
   console.log("Non viene applicato nessuno sconto");
 } else{
-  var sconto40 = Number(prezzoBiglietto - (prezzoBiglietto * (40 / 100)));
+  sconto40;
   console.log(sconto40);
-
-  document.getElementById('totalPrice').innerHTML = sconto40 + " € " + "sconto del 40%";
   console.log("Applicato uno sconto del 40%");
 }
+
+
+// ////PROGRAMMA 2 CONSOLE + HTML////
+// //Variabili km, anni prezzo biglietto
+// var kmDaPercorrere = Number(prompt("numero di chilometri che vuole percorrere"));
+// //console.log(kmDaPercorrere);
+// document.getElementById('kmDaPercorrere').innerHTML = kmDaPercorrere + "km";
+//
+// var userAge = Number(prompt("Quanti anni hai?"));
+// //console.log(userAge);
+// document.getElementById('userAge').innerHTML = userAge + " anni";
+//
+// var prezzoBiglietto = Number(kmDaPercorrere * 0.21);
+// console.log(prezzoBiglietto);
+// document.getElementById('prezzoBiglietto').innerHTML = prezzoBiglietto + " € ";
+//
+// // Variabili sconto minorenni e over 65
+// var sconto20 = Number(prezzoBiglietto - (prezzoBiglietto * (20 / 100)));
+// // console.log(sconto20);
+//
+// var sconto40 = Number(prezzoBiglietto - (prezzoBiglietto * (40 / 100)));
+// // console.log(sconto40);
+//
+//
+// // Variabi dichiarate esternamente al programma + modifica id html interne al programma
+// if(userAge <= 17) {
+//   document.getElementById('totalPrice').innerHTML = sconto20 + " € " + "sconto del 20%";
+//   console.log(sconto20);
+//   console.log("Applicato uno sconto del 20%");
+// } else if(userAge > 17 && userAge < 65) {
+//   document.getElementById('totalPrice').innerHTML = prezzoBiglietto + " €";
+//   console.log(prezzoBiglietto);
+//   console.log("Non viene applicato nessuno sconto");
+// } else{
+//   document.getElementById('totalPrice').innerHTML = sconto40 + " € " + "sconto del 40%";
+//   console.log(sconto40);
+//   console.log("Applicato uno sconto del 40%");
+// }
+
+
+// ////PROGRAMMA 3 CONSOLE + HTML////
+// //Variabili km, anni e prezzo biglietto
+// var kmDaPercorrere = Number(prompt("numero di chilometri che vuole percorrere"));
+// //console.log(kmDaPercorrere);
+// document.getElementById('kmDaPercorrere').innerHTML = kmDaPercorrere + "km";
+//
+// var userAge = Number(prompt("Quanti anni hai?"));
+// //console.log(userAge);
+// document.getElementById('userAge').innerHTML = userAge + " anni";
+//
+// var prezzoBiglietto = Number(kmDaPercorrere * 0.21);
+// console.log(prezzoBiglietto);
+// document.getElementById('prezzoBiglietto').innerHTML = prezzoBiglietto + " € ";
+//
+//
+// // Variabili dichiarate internamente al programma + modifica id html interne al programma
+// if(userAge <= 17) {
+//   var sconto20 = Number(prezzoBiglietto - (prezzoBiglietto * (20 / 100)));
+//   console.log(sconto20);
+//
+//   document.getElementById('totalPrice').innerHTML = sconto20 + " € " + "sconto del 20%";
+//   console.log("Applicato uno sconto del 20%");
+//
+// } else if(userAge > 17 && userAge < 65) {
+//   console.log(prezzoBiglietto);
+//
+//   document.getElementById('totalPrice').innerHTML = prezzoBiglietto + " €";
+//   console.log("Non viene applicato nessuno sconto");
+// } else{
+//   var sconto40 = Number(prezzoBiglietto - (prezzoBiglietto * (40 / 100)));
+//   console.log(sconto40);
+//
+//   document.getElementById('totalPrice').innerHTML = sconto40 + " € " + "sconto del 40%";
+//   console.log("Applicato uno sconto del 40%");
+// }
